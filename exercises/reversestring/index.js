0436048fc7bc04608f6146af373635e4997d94ad
Stefan Dzalev <dzalev@hotmail.com>
2023-02-08 14:01:10 +0100

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,12 +2,37 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-
+    let reversed = '';
+    for (let character of str) {
+        reversed = character + reversed;
+    }
+    return reversed;
 }
+reverse('apple')
 
 module.exports = reverse;
+
+// solution 1
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// solution 2
+// function reverse(str) {
+//     let reversed = '';
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
+
+// solution 3
+//
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev, '');
+// }
